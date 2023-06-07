eba8f4e615aca3aa1216e53368f133be7156f837
Aline <98706488+AyuHamano@users.noreply.github.com>
2023-06-07 20:12:37 -0300

    finalizando a tarefa t08

--- tarefa-t08/src/main/java/org/TextoParagrafo/Sentenca.java
+++ tarefa-t08/src/main/java/org/TextoParagrafo/Sentenca.java
@@ -3,7 +3,7 @@ package org.TextoParagrafo;
 import java.util.ArrayList;
 public class Sentenca {
     ArrayList<String> sentenca = new ArrayList<>();
-    public void setSentenca(ArrayList<String> sentenca) {
+    public void Sentenca(ArrayList<String> sentenca) {
         this.sentenca = sentenca;
     }
 }
