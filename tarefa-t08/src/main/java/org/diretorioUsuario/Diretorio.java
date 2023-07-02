275914136e27b3201ab016021dbc5a804f0e139a
Aline <98706488+AyuHamano@users.noreply.github.com>
2023-07-02 20:34:03 -0300

    finalização da t08 - adição das packages de diretório e usuário

---
+++ tarefa-t08/src/main/java/org/diretorioUsuario/Diretorio.java
@@ -0,0 +1,6 @@
+package org.diretorioUsuario;
+
+public class Diretorio {
+    protected //somente objetos da classe do mesmo package vai ter acesso, ou seja, a classe usuario
+    String path;
+}
---
+++ tarefa-t08/src/main/java/org/diretorioUsuario/Usuario.java
@@ -0,0 +1,11 @@
+package org.diretorioUsuario;
+
+public class Usuario {
+
+    private //somente outros objetos "usuarios" vao ter acesso ao diretorio de outro usuario
+    String nome, login, senha;
+    Diretorio diretorio;
+
+    public
+    Usuario(){}
+}
