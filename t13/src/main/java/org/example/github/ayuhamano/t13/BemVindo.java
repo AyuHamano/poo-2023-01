14fd3a70a2bd50777d96a6a38fc6d1ebeb7e3463
Aline <98706488+AyuHamano@users.noreply.github.com>
2023-07-23 00:20:41 -0300

    Finalização da t13

---
+++ t13/src/main/java/org/example/github/ayuhamano/t13/BemVindo.java
@@ -0,0 +1,15 @@
+package org.example.github.ayuhamano.t13;
+
+
+public class BemVindo {
+    public static void main(String[] args) {
+
+        System.out.printf("Seja bem vindo ao GitHub da Ayu!");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("i = " + i);
+        }
+    }
+}
