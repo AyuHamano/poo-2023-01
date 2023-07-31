a82ee4b694cfd8b8dc76647b1300337e237890b6
AyuHamano <aline_soma@discente.ufg.br>
2023-07-30 23:50:54 -0300

    configuração inicial da t19

---
+++ t17/src/main/java/org/example/github/ayuhamano/t17/trabalhadorProfissao/Acougueiro.java
@@ -0,0 +1,9 @@
+package org.example.github.ayuhamano.t17.trabalhadorProfissao;
+
+public class Advogado extends Profissao {
+    String nome, id, areaAtuacao, graduacao;
+    int numOAB;
+    public Advogado(int numOAB) {
+        this.numOAB = numOAB;
+    }
+}
